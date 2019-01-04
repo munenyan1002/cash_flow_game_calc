f373bec9badc666bb61c287248eaa47b82045b74
munenyan1002 <sitaziki@gmail.com>
2019-01-05 08:05:08 +0900

    topページ、aboutページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/main'
+  get 'top'=>'home#top'
+  get 'about'=>'home#about'
+  get 'main'=>'home#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
