30dafacdeadc1a501d123373e728f5b5a9360d31
munenyan1002 <sitaziki@gmail.com>
2019-01-05 10:31:57 +0900

    画像を追加、topページのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top'=>'home#top'
+  get '/'=>'home#top'
   get 'about'=>'home#about'
   get 'main'=>'home#main'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
